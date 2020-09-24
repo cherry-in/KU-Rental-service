91d343d5b4fe9a3fb980721db823acb4297398cd
최가영 <gyty7@naver.com>
2020-09-24 14:44:19 +0900

    서버

--- server/app.js
+++ server/app.js
@@ -1,20 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const port =3030;
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const connect = require('./schemas');
 
-var app = express();
-
-const port = 3030;
+const app = express();
+connect();
 
 app.listen(port, () => console.log(port));
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(port, () => console.log(port));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
