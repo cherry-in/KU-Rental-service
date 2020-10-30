e991eb081a86a43d9178dbe43e4da33ca0a2510d
YeaJin Ha <hyj1004mu@naver.com>
2020-10-30 14:37:55 +0900

    뭐지

--- server/client/src/Pages/NoticePage.js
+++ server/client/src/Pages/NoticePage.js
@@ -45,11 +45,26 @@ function Notice() {
 
                         <h2 className="p-3 border-bottom">공지사항</h2>
 
-                        <div id="accordion w-90 pt-1">
-                            {notices.map((notice, index) =>
+                        <div id="accordion pt-1">
+                        <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible Group Item #1
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+                            {/* {notices.map((notice, index) =>
                                 <div className="card">
-                                    <div className="card-header collapsed card-link w-100 row m-0 p-1" id={"Hnotice_" + index} data-toggle="collapse" href={"#notice_" + index}>
-                                        <div>
+                                    <div className="card-header" id={"Hnotice_" + index}>
+                                        <div className="row card-link collapsed" data-toggle="collapse" data-target={"#notice_" + index} aria-expanded="false" aria-controls={"notice_" + index}>
                                             <div className="col-6 p-0">{notice.notice_title}</div>
                                             <div className="col-3 p-0 text-center">{notice.notice_author}</div>
                                             <div className="col-3 p-0 text-right">{dateForm(notice.post_date)}</div>
@@ -59,7 +74,7 @@ function Notice() {
                                         <div className="card-body">{notice.notice_content}</div>
                                     </div>
                                 </div>
-                            )}
+                            )} */}
                         </div>
 
                     </div>
