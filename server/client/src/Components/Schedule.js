c1e7b070e17032b06642629f2aefd2d1fc405b30
hijinju <jinju0557@naver.com>
2020-11-02 17:20:18 +0900

    옹

--- server/client/src/Components/Schedule.js
+++ server/client/src/Components/Schedule.js
@@ -3,9 +3,9 @@ import Calendar from '@toast-ui/react-calendar';
 import "tui-calendar/dist/tui-calendar.css";
 import "tui-date-picker/dist/tui-date-picker.css";
 import "tui-time-picker/dist/tui-time-picker.css";
-  
+
 function Cal(calledday) {
-  const calendarRef = useRef();
+  const calendarRef = useRef(null);
   const [day, setDay] = useState(calledday + "15:00:00");
   const [myTheme, setMyTheme] = useState({
     'common.dayname.color': '#333',
@@ -14,6 +14,39 @@ function Cal(calledday) {
     'common.creationGuide.backgroundColor': 'gray',
     // Theme object to extends default dark theme.
   });
+
+  function getDataAction(target) {
+    return target.dataset ? target.dataset.action : target.getAttribute('data-action');
+  }
+
+  function onClickNavi(e) {
+    const cal = calendarRef.current.getInstance();
+    const action = getDataAction(e.target);
+  
+    switch (action) {
+      case 'move-prev':
+        cal.prev();
+        break;
+      case 'move-next':
+        cal.next();
+        break;
+      case 'move-today':
+        cal.today();
+        break;
+      default:
+        return;
+    }
+  
+    // setRenderRangeText();
+    // setSchedules();
+  }
+
+  function useday(day) {
+    const cal = calendarRef.current.getInstance();
+    cal.setDate(new Date(day));
+    cal.changeView('week', true);
+    cal.today(new Date(day));
+  }
 
   useEffect(() => {
     // const cal = calendarRef.current.getInstance();
@@ -42,7 +75,15 @@ function Cal(calledday) {
   }, [day])
 
   return (
+    <div className="container mt-3">
+    <div class="tui-datepicker-input tui-datetime-input tui-has-focus">
+    <input type="text" id="datepicker-input" aria-label="Date-Time"></input>
+    <span class="tui-ico-date"></span>
+</div>
+<div id="wrapper" style="margin-top: -1px;"></div>
+
     <Calendar
+      ref={calendarRef}
       height="100%"
       calendars={[
         {
@@ -105,6 +146,7 @@ function Cal(calledday) {
         hourEnd: 23
       }}
     />
+    </div>
   )
 }
 
--- server/client/src/Pages/HomePage.js
+++ server/client/src/Pages/HomePage.js
@@ -38,7 +38,7 @@ function Home() {
         let weeks = [];
         let dates = [];
         let sendDates = [];
-        
+
         for (let i = 0; i < 6; i++) {
             if (i !== 0) {
                 today.add(7, 'd');
@@ -76,51 +76,52 @@ function Home() {
                         <li>주말: 이용 불가</li>
                     </ul>
                 </p>
-                <Drop className="row dropdown mt-1 mb-2">
-                    <button className="btn btn-lg dropdown-toggle mx-auto col-5" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        기간을 선택해주십시오.
+
+                    <Drop className="row dropdown mt-1 mb-2">
+                        <button className="btn btn-lg dropdown-toggle mx-auto col-5" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            기간을 선택해주십시오.
                     </button>
-                    <div className="dropdown-menu col-5" aria-labelledby="dropdownMenuButton">
-                        {weeks.map((week, index) => (
-                            <a className="dropdown-item" id={index} onClick={(e) => choose(e, index)}>{week}</a>
-                        ))}
+                        <div className="dropdown-menu col-5" aria-labelledby="dropdownMenuButton">
+                            {weeks.map((week, index) => (
+                                <a className="dropdown-item" id={index} onClick={(e) => choose(e, index)}>{week}</a>
+                            ))}
+                        </div>
+                    </Drop>
+                    <Tab className="nav nav-tabs nav-justified mt-4" id="myTab" role="tablist" style={show ? {} : { display: "none" }}>
+                        <li className="nav-item">
+                            <a className="nav-link active" id="7-tab" data-toggle="tab" href="#tab-7" role="tab" aria-controls="7" aria-selected="true"> 7-223 </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="9-tab" data-toggle="tab" href="#tab-9" role="tab" aria-controls="9" aria-selected="false"> 9-116 </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="25-tab" data-toggle="tab" href="#tab-25" role="tab" aria-controls="25" aria-selected="false"> 25-307 </a>
+                        </li>
+                    </Tab>
+                    <div className="tab-content row justify-content-center" id="myTabContent" style={show ? {} : { display: "none" }}>
+                        <div className="tab-pane fade show active col-md-11 col-12" id="tab-7" role="tabpanel" aria-labelledby="7-tab">
+                            <Schedule day={sendDate} />
+                        </div>
+                        <div className="tab-pane fade col-md-11 col-12" id="tab-9" role="tabpanel" aria-labelledby="9-tab">
+                            <Schedule day={sendDate} />
+                        </div>
+                        <div className="tab-pane fade col-md-11 col-12" id="tab-25" role="tabpanel" aria-labelledby="25-tab">
+                            <Schedule day={sendDate} />
+                        </div>
                     </div>
-                </Drop>
-                <Tab className="nav nav-tabs nav-justified mt-4" id="myTab" role="tablist" style={show ? {} : { display: "none" }}>
-                    <li className="nav-item">
-                        <a className="nav-link active" id="7-tab" data-toggle="tab" href="#tab-7" role="tab" aria-controls="7" aria-selected="true"> 7-223 </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" id="9-tab" data-toggle="tab" href="#tab-9" role="tab" aria-controls="9" aria-selected="false"> 9-116 </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" id="25-tab" data-toggle="tab" href="#tab-25" role="tab" aria-controls="25" aria-selected="false"> 25-307 </a>
-                    </li>
-                </Tab>
-                <div className="tab-content row justify-content-center" id="myTabContent" style={show ? {} : { display: "none" }}>
-                    <div className="tab-pane fade show active col-md-11 col-12" id="tab-7" role="tabpanel" aria-labelledby="7-tab">
-                        <Schedule day={sendDate}/>
+                    <h3>유의사항</h3>
+                    <p>
+                        <ul>
+                            <li>강의실을 대관하는 대표자를 기준으로 최대 주 6시간까지 대관이 가능합니다.</li>
+                            <li>1회 대관시 최대 3시간까지 이용이 가능합니다. (1시간 단위로 대관 가능)</li>
+                            <li><strong style={{ color: "red" }}>대관 시간 이외 강의실을 이용하다 적발될 경우 한달 간 강의실 이용이 불가합니다.</strong></li>
+                        </ul>
+                    </p>
+                    <div className="bg-white">
+                        <h6>고려대학교 대관시스템</h6>
                     </div>
-                    <div className="tab-pane fade col-md-11 col-12" id="tab-9" role="tabpanel" aria-labelledby="9-tab">
-                        <Schedule day={sendDate}/>
-                    </div>
-                    <div className="tab-pane fade col-md-11 col-12" id="tab-25" role="tabpanel" aria-labelledby="25-tab">
-                        <Schedule day={sendDate}/>
-                    </div>
-                </div>
-                <h3>유의사항</h3>
-                <p>
-                    <ul>
-                        <li>강의실을 대관하는 대표자를 기준으로 최대 주 6시간까지 대관이 가능합니다.</li>
-                        <li>1회 대관시 최대 3시간까지 이용이 가능합니다. (1시간 단위로 대관 가능)</li>
-                        <li><strong style={{ color: "red" }}>대관 시간 이외 강의실을 이용하다 적발될 경우 한달 간 강의실 이용이 불가합니다.</strong></li>
-                    </ul>
-                </p>
-                <div className="bg-white">
-                    <h6>고려대학교 대관시스템</h6>
                 </div>
             </div>
-        </div>
     )
 }
 
